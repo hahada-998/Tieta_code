d3e4f0f9c06c36b4ac64b916741dfe2329a436ad
hahada-123 <13345218908@163.com>
2025-12-29 19:29:49 +0800

    🦄 refactor(主板主逻辑上电编码):

--- Tieta_Feiteng_1001_PGC7KD69MBG400/rtl/source/Tieta_Feiteng_1001_top.v
+++ Tieta_Feiteng_1001_PGC7KD69MBG400/rtl/source/Tieta_Feiteng_1001_top.v
@@ -3,7 +3,26 @@
 
 parameter PEAVEY_SUPPORT = 1'b1;  //comment1,ph configuration
 
-/*
+/*------------------------------------------------------------------
+PLL 例化 
+--------------------------------------------------------------------*/
+pll_i25M_o50M_o25M pll_inst(
+  .CLKI  (i_CLK_PAL_IN_25M          ),
+  .RST   (~i_PAL_P3V3_STBY_PGD      ),
+  .CLKOP (clk_100m                  ),
+  .CLKOS (pll_clkos1                ),
+  .LOCK  (pll_lock                  )
+);
 
-*/
+/*------------------------------------------------------------------
+PLL 例化 
+--------------------------------------------------------------------*/
+pll_i25M_o50M_o25M pll_inst(
+  .CLKI  (i_CLK_PAL_IN_25M          ),
+  .RST   (~i_PAL_P3V3_STBY_PGD      ),
+  .CLKOP (clk_100m                  ),
+  .CLKOS (pll_clkos1                ),
+  .LOCK  (pll_lock                  )
+);
+
 endmodule
