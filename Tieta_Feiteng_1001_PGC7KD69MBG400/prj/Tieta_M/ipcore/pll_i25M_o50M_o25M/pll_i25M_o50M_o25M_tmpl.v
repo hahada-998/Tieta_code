df90f442cf652489de01dd4c09cc282d0ec08fde
hahada-123 <13345218908@163.com>
2025-12-30 20:35:01 +0800

    🦄 refactor(电源上下电编码):

---
+++ Tieta_Feiteng_1001_PGC7KD69MBG400/prj/Tieta_M/ipcore/pll_i25M_o50M_o25M/pll_i25M_o50M_o25M_tmpl.v
@@ -0,0 +1,15 @@
+// Created by IP Generator (Version 2024.2-SP1.2 build 187561)
+// Instantiation Template
+//
+// Insert the following codes into your Verilog file.
+//   * Change the_instance_name to your own instance name.
+//   * Change the signal names in the port associations
+
+
+pll_i25M_o50M_o25M the_instance_name (
+  .clkout0(clkout0),    // output 50.00000000MHz
+  .clkout1(clkout1),    // output 25.00000000MHz
+  .lock(lock),          // output
+  .clkin1(clkin1),      // input 25.0000MHz
+  .rst(rst)             // input
+);
